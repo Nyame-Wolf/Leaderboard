02ac4c2905c7498b01e61351bc9b1bcaba819354
Nyame-Wolf <mumenyam@gmail.com>
2022-07-22 09:56:30 +0300

    modify for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    // path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/leaderboard/',
     clean: true,
   },
